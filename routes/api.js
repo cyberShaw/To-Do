85d63c3dbcfe4b47eb9dee90d6fa2ae8c386e080
Saiharsha B <34317373+thewhitewolf17@users.noreply.github.com>
2019-06-17 20:55:51 +0530

    Add API routes for To-Do

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/todos',(req, res, next) => {
+
+});
+
+router.post('/todos', (req, res, next) => {
+
+});
+
+router.delete('/todos', (req, res, next) => {
+
+});
